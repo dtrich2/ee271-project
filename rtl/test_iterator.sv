b7dc16473df1c01d6884394d87898544cc7b0ca7
Chris-Not-Mikey <54165966+Chris-Not-Mikey@users.noreply.github.com>
2021-11-28 10:26:28 -0800

    Update test_iterator.sv

--- rtl/test_iterator.sv
+++ rtl/test_iterator.sv
@@ -266,7 +266,7 @@ if(MOD_FSM == 0) begin // Using baseline FSM
     logic                       at_right_edg_R14H;      //Current sample at right edge of bbox?
     logic                       at_top_edg_R14H;        //Current sample at top edge of bbox?
     logic                       at_end_box_R14H;        //Current sample at end of bbox?
-    logic [3:0]                 subSample_RnnnnU_MSAA;  //subSample with modifided to MSAA requirements
+    logic [SAMPS-1:0]             subSample_RnnnnU_MSAA;  //subSample with modifided to MSAA requirements
 
     //////
     ////// First calculate the values of the helper signals using CURRENT STATES
@@ -283,7 +283,7 @@ if(MOD_FSM == 0) begin // Using baseline FSM
         // START CODE HERE
 
          //Set next right samples
-       subSample_RnnnnU_MSAA = subSample_RnnnnU << RADIX-3;
+       subSample_RnnnnU_MSAA = (subSample_RnnnnU << RADIX-3_;
      
        for (int i=0; i<SAMPS ; i++) begin 
            next_rt_samp_R14S[0][i] = sample_R14S[0][i]  + subSample_RnnnnU_MSAA;
@@ -293,7 +293,7 @@ if(MOD_FSM == 0) begin // Using baseline FSM
            next_up_samp_R14S[0][i] =  box_R14S[0][0]; //Back to min x coord
            //increment by 4
            //TODO: Do a shift multiplication
-           next_up_samp_R14S[1][i] = sample_R14S[1][i] + (subSample_RnnnnU_MSAA << SAMP_SHIFT) ;                                       
+           next_up_samp_R14S[1][i] = sample_R14S[1][i] + ((subSample_RnnnnU << RADIX-3) << SAMP_SHIFT) ;                                       
            
        end
 
