108640a1c3223ae7c5cb67c89b060fb2651d34d5
Chris-Not-Mikey <54165966+Chris-Not-Mikey@users.noreply.github.com>
2021-11-27 23:14:33 -0800

    Update hash_jtree.sv

--- rtl/hash_jtree.sv
+++ rtl/hash_jtree.sv
@@ -296,8 +296,8 @@ module hash_jtree
 
     dff3 #(
         .WIDTH(SIGFIG),
-        .ARRAY_SIZE(2),
         .ARRAY_SIZE(4),
+        .ARRAY_SIZE(2),
         .PIPE_DEPTH(1),
         .RETIME_STATUS(0)
     )
