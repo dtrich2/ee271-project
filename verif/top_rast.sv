e467bab046eb00181c5944b921ef6eb675dac560
Chris-Not-Mikey <54165966+Chris-Not-Mikey@users.noreply.github.com>
2021-11-27 21:34:11 -0800

    Update top_rast.sv

--- verif/top_rast.sv
+++ verif/top_rast.sv
@@ -60,9 +60,9 @@ module top_rast;
 
     //DUT OUTPUTS
     logic                       halt_RnnnnL;
-    logic signed   [SIGFIG-1:0] hit_R18S[AXIS-1:0]; //Sample hit Location
+    logic signed   [SIGFIG-1:0] hit_R18S[AXIS-1:0][3:0]; //Sample hit Location
     logic unsigned [SIGFIG-1:0] color_R18U[COLORS-1:0]; //Sample hit Location
-    logic                       hit_valid_R18H ;  //Did Sample Hit?
+    logic                       hit_valid_R18H[3:0] ;  //Did Sample Hit?
     //DUT OUTPUTS
 
     // instantiate the DUT
