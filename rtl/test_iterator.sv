fd71a8ff4ef99c2665a61d85ac2d556b8c0e3960
Chris-Not-Mikey <54165966+Chris-Not-Mikey@users.noreply.github.com>
2021-11-29 18:20:51 -0800

    Update test_iterator.sv

--- rtl/test_iterator.sv
+++ rtl/test_iterator.sv
@@ -246,7 +246,7 @@ if(MOD_FSM == 0) begin // Using baseline FSM
     (
         .clk    (clk            ),
         .reset  (rst            ),
-        .en     (validTri_R13H          ),
+        .en     (1'b1          ), //try valid triangle
         .in     (next_box_R14S  ),
         .out    (box_R14S       )
     );
