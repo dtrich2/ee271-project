a7f410fabf7630d65d96c1f32854c1c4d11b7519
Chris-Not-Mikey <54165966+Chris-Not-Mikey@users.noreply.github.com>
2021-11-29 18:54:46 -0800

    Update sampletest.sv

--- rtl/sampletest.sv
+++ rtl/sampletest.sv
@@ -178,7 +178,7 @@ module sampletest
     (
         .clk    (clk            ),
         .reset  (rst            ),
-        .en     (valid_gate      ),
+        .en     (1'b1     ),
         .in     (hit_R16S       ),
         .out    (hit_R18S_retime)
     );
@@ -193,7 +193,7 @@ module sampletest
     (
         .clk    (clk                ),
         .reset  (rst                ),
-        .en     (1'b1             ),
+        .en     (valid_gate            ),
         .in     (color_R16U         ),
         .out    (color_R18U_retime  )
     );
